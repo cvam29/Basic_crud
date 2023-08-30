03ce835a5e7ff5cb6fc9cd29a0ce83d828157f54
shivam <shivam@southpawstudio.co>
2023-08-30 11:31:48 +0530

    Initialized Email with empty

--- Basic_crud/Models/Customer.cs
+++ Basic_crud/Models/Customer.cs
@@ -7,9 +7,9 @@ namespace Basic_crud.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; } =string.Empty;
+        public string Name { get; set; } = string.Empty;
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         [Required]
         public string City { get; set; }
         [Required]
